616db27665a369fe75876957091af205aea45303
sg0017473 <sg0017473@INCDSZLP00147.TechMahindra.com>
2020-01-22 12:47:32 +0530

    First Push

---
+++ src/test/java/package1/TestClass1.java
@@ -0,0 +1,22 @@
+package package1;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+@Test
+public void Testcase1()
+{
+	System.setProperty("webdriver.chrome.driver", "D:\\selenium\\Driver\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("http://3.19.255.165:8080/DevOps/");
+	String actual="Calculator for DevOps";
+	String expected=driver.getTitle();
+	Assert.assertEquals(actual, expected);//.assertEquals(, title );
+	
+}
+}
