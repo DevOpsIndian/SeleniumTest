4c0993fe5d1d2d0977c81629dbadcec3663e4adb
sg0017473 <sg0017473@INCDSZLP00147.TechMahindra.com>
2020-01-22 14:40:51 +0530

    Changed

--- src/test/java/package1/TestClass1.java
+++ src/test/java/package1/TestClass1.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.assertEquals;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,8 +12,13 @@ public class TestClass1 {
 @Test
 public void Testcase1()
 {
-	System.setProperty("webdriver.chrome.driver", "D:\\selenium\\Driver\\chromedriver.exe");
-	WebDriver driver = new ChromeDriver();
+	//System.setProperty("webdriver.chrome.driver", "D:\\selenium\\Driver\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+    ChromeOptions chromeOptions = new ChromeOptions();
+    chromeOptions.addArguments("--headless");
+    chromeOptions.addArguments("--no-sandbox");
+
+	WebDriver driver = new ChromeDriver(chromeOptions);
 	driver.get("http://3.19.255.165:8080/DevOps/");
 	String actual="Calculator for DevOps";
 	String expected=driver.getTitle();
